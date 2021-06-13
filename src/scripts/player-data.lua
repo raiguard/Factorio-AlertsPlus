7fd08066b74502821e9f4a3cb37c756a58e22c9d
raiguard <caleb.heuer@gmail.com>
2021-06-12 21:20:53 -0600

    Group alerts instead of showing individual ones

--- src/scripts/player-data.lua
+++ src/scripts/player-data.lua
@@ -27,27 +27,36 @@ function player_data.update_game_alerts(player, player_table)
   local gui_data = player_table.guis.dash
   if not gui_data then return end
 
+  -- local player_alerts = player_table.alerts
+  local compiled_alerts = {}
+  for surface_index, alerts in pairs(player.get_alerts{}) do
+    local surface_alerts = {}
+    for alert_type, alerts in pairs(alerts) do
+      local type_key = reverse_defines.alert_type[alert_type]
+      local i = 0
+      for _ in pairs(alerts) do
+        i = i + 1
+      end
+      if i > 0 then
+        surface_alerts[type_key] = i
+      end
+    end
+    if table_size(surface_alerts) > 0 then
+      compiled_alerts[game.get_surface(surface_index).name] = surface_alerts
+    end
+  end
+
   local refs = gui_data.refs
   local pane = refs.list_pane
   pane.clear()
 
-  -- local player_alerts = player_table.alerts
-  -- local compiled_alerts = {}
-  for surface_index, alerts in pairs(player.get_alerts{}) do
-    -- window.add{type = "label", style = "caption_label", caption = game.surfaces[surface_index].name}
-    for alert_type, alerts in pairs(alerts) do
-      for _, alert_data in pairs(alerts) do
-        pane.add{
-          type = "label",
-          caption = reverse_defines.alert_type[alert_type]
-            .." "
-            ..(
-              alert_type == defines.alert_type.custom
-                and "[img="..alert_data.icon.type.."/"..alert_data.icon.name.."]  "..alert_data.message
-                or ""
-            )
-        }
-      end
+  for surface_name, surface_alerts in pairs(compiled_alerts) do
+    pane.add{type = "label", style = "caption_label", caption = surface_name}
+    for alert_type, number in pairs(surface_alerts) do
+      pane.add{
+        type = "label",
+        caption = alert_type.." ("..number..")",
+      }
     end
   end
 end
